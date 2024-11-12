5be33fad302a9576a12fdad092448bfbd8f808d4
Julnowak <julnowak@student.agh.edu.pl>
2024-11-12 14:59:55 +0100

    bp

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Container, Navbar, Nav, Row, Col, Card, Form, Button} from 'react-bootstrap';
+import {Container, Navbar, Nav, Row, Col, Card, Form, Button, Carousel} from 'react-bootstrap';
 import './Header.css';
 import logo from '../../assets/weles_white.png';
 import PhotoCarousel from "../PhotoCarousel/PhotoCarousel";
@@ -14,11 +14,6 @@ const Homepage = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-    const positions = [
-        { name: 'Kraków', lat: 50.0647, lng: 19.9450 },
-        { name: 'Warszawa', lat: 52.2298, lng: 21.0118 },
-        { name: 'Poznań', lat: 52.4084, lng: 16.9342 },
-      ];
     const photos = [];
 
     for (let i = 1; i <= 5; i++) {
@@ -87,51 +82,93 @@ const Homepage = () => {
 
                 <section id="gallery" className="mb-5">
                     <h2>Nasze hotele</h2>
-                    <Row xs={1} md={2} lg={3} className="g-4">
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://lh3.googleusercontent.com/DTbCvkAUEJUs4tNNRqW0N6w68ENyw9y6GjCr9dsxRFDU1Gv4SBjpY5dm8WlF8G8Acnt5oj5cZRXWDLriA9IwCbDqoo8bJbNcVI3BdAzH720wxj1sQbuAHtTebTgZMY5GHs-uXDWa"/>
-                                <Card.Body>
-                                    <Card.Title>Pokój 1</Card.Title>
-                                    <Card.Text>Luksusowy pokój z widokiem.</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6egZUirq7hHJ_gNZaLG124bxxgu0rirsFVw&s"/>
-                                <Card.Body>
-                                    <Card.Title>Pokój 2</Card.Title>
-                                    <Card.Text>Przytulny i komfortowy.</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top"
-                                          src="https://lubiepodroze.eu/wp-content/uploads/2019/12/Courtyard-by-Marriott-Warsaw-Airport-Sypialnia-w-apartamencie.jpg"/>
-                                <Card.Body>
-                                    <Card.Title>Pokój 3</Card.Title>
-                                    <Card.Text>Stylowy pokój dla par.</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <div style={{color: "black", textAlign: "right", paddingRight: 20}}>
-                            <a href={"/hotels"}>
-                                Zobacz więcej...
-                            </a>
+                    <Carousel>
+                        <Carousel.Item>
+                            <div className="d-block w-100" style={{position: 'relative'}}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://lh3.googleusercontent.com/DTbCvkAUEJUs4tNNRqW0N6w68ENyw9y6GjCr9dsxRFDU1Gv4SBjpY5dm8WlF8G8Acnt5oj5cZRXWDLriA9IwCbDqoo8bJbNcVI3BdAzH720wxj1sQbuAHtTebTgZMY5GHs-uXDWa"
+                                    alt="Pokój 1"
+                                />
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        transform: 'translate(-50%, -50%)',
+                                        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                                        padding: '10px 20px',
+                                        color: 'white',
+                                        borderRadius: '5px',
+                                    }}
+                                >
+                                    <h3>Pokój 1</h3>
+                                    <p>Luksusowy pokój z widokiem.</p>
+                                </div>
+                            </div>
+                        </Carousel.Item>
 
-                        </div>
-                    </Row>
+                        <Carousel.Item>
+                            <div className="d-block w-100" style={{position: 'relative'}}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6egZUirq7hHJ_gNZaLG124bxxgu0rirsFVw&s"
+                                    alt="Pokój 2"
+                                />
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        transform: 'translate(-50%, -50%)',
+                                        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                                        padding: '10px 20px',
+                                        color: 'white',
+                                        borderRadius: '5px',
+                                    }}
+                                >
+                                    <h3>Pokój 2</h3>
+                                    <p>Przytulny i komfortowy.</p>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <div className="d-block w-100" style={{position: 'relative'}}>
+                                <img
+                                    className="d-block w-100"
+                                    src="https://lubiepodroze.eu/wp-content/uploads/2019/12/Courtyard-by-Marriott-Warsaw-Airport-Sypialnia-w-apartamencie.jpg"
+                                    alt="Pokój 3"
+                                />
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        transform: 'translate(-50%, -50%)',
+                                        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                                        padding: '10px 20px',
+                                        color: 'white',
+                                        borderRadius: '5px',
+                                    }}
+                                >
+                                    <h3>Pokój 3</h3>
+                                    <p>Stylowy pokój dla par.</p>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
+
+                    <div style={{color: 'black', textAlign: 'right', paddingRight: 20}}>
+                        <a href="/hotels">Zobacz więcej...</a>
+                    </div>
                 </section>
 
                 <section id="gallery" className="mb-5">
                     <h2>Galeria</h2>
                     <PhotoCarousel photos={photos}/>
                     <div style={{color: "black", textAlign: "right", paddingRight: 20}}>
-                        <a href={"/gallery"} >
+                        <a href={"/gallery"}>
                             Zobacz więcej...
                         </a>
                     </div>
