b562ab96bf40ac0548d8476e9cd1dc7e28f262da
Julnowak <julnowak@student.agh.edu.pl>
2025-01-14 08:03:57 +0100

    small fixes

--- frontend/src/components/ReservationManagement/ReservationManagement.js
+++ frontend/src/components/ReservationManagement/ReservationManagement.js
@@ -330,13 +330,13 @@ const ReservationManagement = () => {
                                         <span style={{marginLeft: -2, marginTop: 5}}>Opłacona</span>
                                     </div>
                                     :
-                                    <div>
+                                    <div style={{display: "inline", marginLeft: 30}}>
                                         <svg color="red" xmlns="http://www.w3.org/2000/svg" width="32" height="32"
                                              fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                                             <path
                                                 d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                                         </svg>
-                                        <span>Nieopłacona</span>
+                                        <span style={{marginLeft: -2, marginTop: 5}}>Nieopłacona</span>
                                     </div>
                                 }
 
