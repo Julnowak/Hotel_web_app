76b2a43c770eba1ddf22f8df2906cb2491237e40
Julnowak <julnowak@student.agh.edu.pl>
2025-01-14 08:11:01 +0100

    Update HotelPage.js

--- frontend/src/components/HotelPage/HotelPage.js
+++ frontend/src/components/HotelPage/HotelPage.js
@@ -252,54 +252,54 @@ const HotelPage = () => {
                 </div>
             </div>
 
-                    <div style={{marginTop: 40}}>
-                        <h4 style={{textAlign: "center", fontWeight: "bold", marginBottom: "20px"}}>
-                            Dodatkowe usługi
-                        </h4>
+            <div style={{marginTop: 40}}>
+                <h4 style={{textAlign: "center", fontWeight: "bold", marginBottom: "20px"}}>
+                    Dodatkowe usługi
+                </h4>
 
-                        <div style={{
-                            display: 'flex',
-                            justifyContent: 'space-around',
-                            alignItems: 'center',
-                            gap: '20px'
-                        }}>
-                            <div style={{textAlign: 'center'}}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                                     className="bi bi-cup-hot-fill" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd"
-                                          d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6zM13 12.5a2 2 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5"/>
-                                    <path
-                                        d="m4.4.8-.003.004-.014.019a4 4 0 0 0-.204.31 2 2 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.6.6 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3 3 0 0 1-.202.388 5 5 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4 4 0 0 0 .204-.31 2 2 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.6.6 0 0 0-.09-.252A4 4 0 0 0 3.6 2.8l-.01-.012a5 5 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a6 6 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8"/>
-                                </svg>
-                                <div>Śniadanie (40 zł/doba/os)</div>
-                            </div>
-
-                            <div style={{textAlign: 'center'}}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                                     className="bi bi-car-front-fill" viewBox="0 0 16 16">
-                                    <path
-                                        d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2m10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17s3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/>
-                                </svg>
-                                <div>Parking (50 zł/doba)</div>
-                            </div>
-
-                            <div style={{textAlign: 'center'}}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                                     className="bi bi-wifi" viewBox="0 0 16 16">
-                                    <path
-                                        d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.44 12.44 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.52.52 0 0 0 .668.05A11.45 11.45 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049"/>
-                                    <path
-                                        d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.46 9.46 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065m-2.183 2.183c.226-.226.185-.605-.1-.75A6.5 6.5 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.5 5.5 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091zM9.06 12.44c.196-.196.198-.52-.04-.66A2 2 0 0 0 8 11.5a2 2 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z"/>
-                                </svg>
-                                <div>Wi-Fi (bezpłatne)</div>
-                            </div>
-                        </div>
-
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    gap: '20px'
+                }}>
+                    <div style={{textAlign: 'center'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                             className="bi bi-cup-hot-fill" viewBox="0 0 16 16">
+                            <path fillRule="evenodd"
+                                  d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6zM13 12.5a2 2 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5"/>
+                            <path
+                                d="m4.4.8-.003.004-.014.019a4 4 0 0 0-.204.31 2 2 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.6.6 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3 3 0 0 1-.202.388 5 5 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4 4 0 0 0 .204-.31 2 2 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.6.6 0 0 0-.09-.252A4 4 0 0 0 3.6 2.8l-.01-.012a5 5 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a6 6 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8"/>
+                        </svg>
+                        <div>Śniadanie (40 zł/doba/os)</div>
                     </div>
+
+                    <div style={{textAlign: 'center'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                             className="bi bi-car-front-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2m10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17s3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/>
+                        </svg>
+                        <div>Parking (50 zł/doba)</div>
+                    </div>
+
+                    <div style={{textAlign: 'center'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                             className="bi bi-wifi" viewBox="0 0 16 16">
+                            <path
+                                d="M15.384 6.115a.485.485 0 0 0-.047-.736A12.44 12.44 0 0 0 8 3C5.259 3 2.723 3.882.663 5.379a.485.485 0 0 0-.048.736.52.52 0 0 0 .668.05A11.45 11.45 0 0 1 8 4c2.507 0 4.827.802 6.716 2.164.205.148.49.13.668-.049"/>
+                            <path
+                                d="M13.229 8.271a.482.482 0 0 0-.063-.745A9.46 9.46 0 0 0 8 6c-1.905 0-3.68.56-5.166 1.526a.48.48 0 0 0-.063.745.525.525 0 0 0 .652.065A8.46 8.46 0 0 1 8 7a8.46 8.46 0 0 1 4.576 1.336c.206.132.48.108.653-.065m-2.183 2.183c.226-.226.185-.605-.1-.75A6.5 6.5 0 0 0 8 9c-1.06 0-2.062.254-2.946.704-.285.145-.326.524-.1.75l.015.015c.16.16.407.19.611.09A5.5 5.5 0 0 1 8 10c.868 0 1.69.201 2.42.56.203.1.45.07.61-.091zM9.06 12.44c.196-.196.198-.52-.04-.66A2 2 0 0 0 8 11.5a2 2 0 0 0-1.02.28c-.238.14-.236.464-.04.66l.706.706a.5.5 0 0 0 .707 0l.707-.707z"/>
+                        </svg>
+                        <div>Wi-Fi (bezpłatne)</div>
+                    </div>
+                </div>
+
+            </div>
             <section style={{marginTop: 40}}>
-                    <h4 style={{textAlign: "center", fontWeight: "bold", marginBottom: "20px"}}>
-                        Lokalizacja
-                    </h4>
+                <h4 style={{textAlign: "center", fontWeight: "bold", marginBottom: "20px"}}>
+                    Lokalizacja
+                </h4>
 
                 <MapContainer
                     center={[hotel.latitude, hotel.longitude]}
@@ -332,7 +332,7 @@ const HotelPage = () => {
                             >
                                 Hotel Weles {hotel?.localization}
                             </h4>
-                            <div style={{ paddingBottom: 10}}>{hotel?.address} {hotel?.localization}</div>
+                            <div style={{paddingBottom: 10}}>{hotel?.address} {hotel?.localization}</div>
                             <StarRating rating={hotel.rating}/>
                             <p style={{fontSize: "14px", color: "black"}}>
                                 Godziny otwarcia:
@@ -416,51 +416,52 @@ const HotelPage = () => {
                         {reviews?.length === 0 ? (
                             <p style={{textAlign: "center", color: "#666", fontStyle: "italic"}}>
                                 Brak recenzji hotelu. Bądź pierwszym, który oceni nasz hotel!
-                            </p>) : (reviews?.map((review, index) => (<div
-                            key={index}
-                            style={{
-                                display: "flex",
-                                padding: "15px",
-                                borderTop: "2px solid lightgray",
-                                borderBottom: "2px solid lightgray",
-                                borderRadius: "10px",
-                                marginBottom: "10px",
-                            }}
-                        >
-                            {/* Profile Picture */}
-                            <img
-                                src={review.user?.profile_picture?.slice(15) || "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"}
-                                alt="Profile"
-                                style={{
-                                    width: "50px",
-                                    height: "50px",
-                                    borderRadius: "50%",
-                                    objectFit: "cover",
-                                    marginRight: "15px",
-                                    border: "2px solid #ccc",
-                                }}
-                            />
-                            {/* Review Content */}
-                            <div style={{flex: 1}}>
-                                <div style={{
-                                    display: "flex", justifyContent: "space-between", alignItems: "center",
-                                }}>
-                                    <div>
-                                        <h5 style={{margin: 0, fontWeight: "bold", color: "#444"}}>
-                                            {review.user?.username || "Anonimowy Użytkownik"}
-                                        </h5>
-                                        <small
-                                            style={{display: "block", color: "#888", marginBottom: "8px"}}>
-                                            Dodano: {new Date(review.created_at).toLocaleString().slice(0, -3) || "Brak daty"}
-                                        </small>
+                            </p>) : (reviews
+                            ?.slice((currentPage - 1) * reviewsPerPage, currentPage * reviewsPerPage)
+                            .map((review, index) => (<div key={index}
+                                                          style={{
+                                                              display: "flex",
+                                                              padding: "15px",
+                                                              borderTop: "2px solid lightgray",
+                                                              borderBottom: "2px solid lightgray",
+                                                              borderRadius: "10px",
+                                                              marginBottom: "10px",
+                                                          }}
+                            >
+                                {/* Profile Picture */}
+                                <img
+                                    src={review.user?.profile_picture?.slice(15) || "https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"}
+                                    alt="Profile"
+                                    style={{
+                                        width: "50px",
+                                        height: "50px",
+                                        borderRadius: "50%",
+                                        objectFit: "cover",
+                                        marginRight: "15px",
+                                        border: "2px solid #ccc",
+                                    }}
+                                />
+                                {/* Review Content */}
+                                <div style={{flex: 1}}>
+                                    <div style={{
+                                        display: "flex", justifyContent: "space-between", alignItems: "center",
+                                    }}>
+                                        <div>
+                                            <h5 style={{margin: 0, fontWeight: "bold", color: "#444"}}>
+                                                {review.user?.username || "Anonimowy Użytkownik"}
+                                            </h5>
+                                            <small
+                                                style={{display: "block", color: "#888", marginBottom: "8px"}}>
+                                                Dodano: {new Date(review.created_at).toLocaleString().slice(0, -3) || "Brak daty"}
+                                            </small>
+                                        </div>
+                                        <StarRating rating={review.rating}/>
                                     </div>
-                                    <StarRating rating={review.rating}/>
+                                    <p style={{margin: "5px 0 0", color: "#555", lineHeight: "1.5"}}>
+                                        {review.description || "Brak opisu dla tej recenzji."}
+                                    </p>
                                 </div>
-                                <p style={{margin: "5px 0 0", color: "#555", lineHeight: "1.5"}}>
-                                    {review.description || "Brak opisu dla tej recenzji."}
-                                </p>
-                            </div>
-                        </div>)))}
+                            </div>)))}
                     </div>
                 </div>
 
